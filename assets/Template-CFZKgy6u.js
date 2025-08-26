24ce16f594686989b1e5d0b5680774643338b07f
Evan Leach <evan.leach@gmail.com>
2025-08-26 22:37:24 +0200

    Fixed arrow urls

---
+++ assets/Template-CFZKgy6u.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-BLtDP95R.js";import{B as r}from"./BaseScene-DQ5BBuqJ.js";const a={steps:["Steps"]};function f({windowSize:e,stepIndex:t,isPanelOpen:s,sceneKey:o}){return n.jsx(r,{config:a,sceneKey:o,stepIndex:t,isPanelOpen:s,windowSize:e})}export{a as SceneConfig,f as default};
